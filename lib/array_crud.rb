23c31ac783cd3eb590f8947da02ba712ef64e0e0
Casey (Bevin) Curry <c.larsoncurry425@gmail.com>
2019-08-07 07:34:47 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,7 @@ end
 
 def add_element_to_end_of_array(array, element)
   friends = ["Angie", "Casey", "Onyx", "Charlyne"]
-  friends << "Jimmy"
+  friends << "Jimmy!"
 end
 
 def add_element_to_start_of_array(array, element)
