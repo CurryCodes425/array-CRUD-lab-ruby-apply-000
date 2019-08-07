6e5d19fd068978f45ff66c903a3d217b8f5cfa45
Casey (Bevin) Curry <c.larsoncurry425@gmail.com>
2019-08-07 08:00:28 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -23,7 +23,7 @@ end
 
 def remove_element_from_start_of_array(array)
   phrase = ["I", "love", "learning", "about", "arrays!"]
-  phrase.shift[0]
+  phrase.shift()
 end
 
 def retrieve_element_from_index(array, index_number)
