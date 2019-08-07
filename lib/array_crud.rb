0d174813e32380340020e9725dc661369ebdab6a
Casey (Bevin) Curry <c.larsoncurry425@gmail.com>
2019-08-07 07:36:07 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,8 +7,8 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  friends = ["Angie", "Casey", "Onyx", "Charlyne"]
-  friends << "Jimmy!"
+  phrase =["I", "love", "learning", "about"]
+  phrase << "arrays!"
 end
 
 def add_element_to_start_of_array(array, element)
