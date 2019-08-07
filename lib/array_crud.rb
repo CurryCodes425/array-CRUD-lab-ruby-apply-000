be59b1e0531944ad576c1a86262974619af22564
Casey (Bevin) Curry <c.larsoncurry425@gmail.com>
2019-08-07 08:07:44 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -32,7 +32,8 @@ def retrieve_element_from_index(array, index_number)
 end
 
 def retrieve_first_element_from_array(array)
-  phrase = ["I", "love", "learning", "about", "arrays!"]
+  phrase = ["wow", "I", "am", "really", "learning", "arrays!"]
+  phrase.index[0]
 end
 
 def retrieve_last_element_from_array(array)
